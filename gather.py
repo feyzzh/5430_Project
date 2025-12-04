e2bc5f331d24b49c7254061b910f42bb2817d5b6
ket6cc <ket6cc@virginia.edu>
2025-12-03 21:34:05 -0500

    data experimentation

--- gather.py
+++ gather.py
@@ -3,6 +3,7 @@ import yfinance as yf
 # https://ericpien.github.io/yfinance/reference/api/methods/yfinance.Tickers.history.html
 # Tickers.history(period='1mo', interval='1d', start=None, end=None, prepost=False, actions=True, auto_adjust=True, repair=False, proxy=None, threads=True, group_by='column', progress=True, timeout=10, **kwargs)
 # Period must be one of: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max
+# Valid intervals: [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 4h, 1d, 5d, 1wk, 1mo, 3mo]
 
 def gather_data(symbol, pd='5y'):
     ticker = yf.Ticker(symbol)
@@ -10,10 +11,11 @@ def gather_data(symbol, pd='5y'):
     data.to_csv(f'{symbol.lower()}_prices.csv')
 
 
-gather_data('MDB')
-gather_data('AAPL')
-gather_data('TSLA')
-gather_data('META')
-gather_data('NVDA')
-gather_data('CRWD')
-gather_data('SPY')
+if __name__ == "__main__":
+    gather_data('MDB')
+    gather_data('AAPL')
+    gather_data('TSLA')
+    gather_data('META')
+    gather_data('NVDA')
+    gather_data('CRWD')
+    gather_data('SPY')
